a12e826de6d70869558dd28d955730c36f2fdbb9
aricrowe57 <arcrowe@microsoft.com>
2022-09-29 22:36:41 -0500

    Update aks-cluster.tf

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -4,6 +4,10 @@ terraform {
         storage_account_name = "arcroweterraformstate"
         container_name       = "tfstate"
         key                  = "local.terraform.tfstate"
+        subscription_id      = "e1fec9f3-3d89-4113-8eaf-d8915babcf59"
+        tenant_id            = "c894abff-2699-4efc-a196-8e1565ec8b93"
+        client_id            = "cfb23b33-17f6-475c-b1b0-f3cfaeb5446a"
+        use_oidc             = true
     }
 
 }
