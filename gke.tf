e639696dcea5e1526a2cad6f5a1d2d5cdab7790e
Arielle Crowe <arcrowe@microsoft.com>
2022-10-06 13:25:04 -0700

    fix destroy and reprovision

--- gke.tf
+++ gke.tf
@@ -4,6 +4,11 @@ resource "google_container_cluster" "default" {
   
   initial_node_count       = 2
 
+ ip_allocation_policy {
+    cluster_ipv4_cidr_block       = "10.104.0.0/14"
+    services_ipv4_cidr_block      = "10.108.0.0/20"
+}
+
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
 }
